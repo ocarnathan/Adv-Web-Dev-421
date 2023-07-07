1f0869279e58e18d26403f724250505f83ab6309
Obie Carnathan <aikia16@uab.edu>
2023-07-07 13:43:49 -0500

    done

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -5,12 +5,11 @@ import re
 url ="http://michaelgathara.com/api/python-challenge"
 
 # Send a GET request to retrieve the challenge
-
 response = requests.get(url)
 
 #Extract the challenges from the response
-
 challenges = response.json()
 
+print("Name: Obie Carnathan\nBlazer ID: aikia16")
 for equation in challenges:
     print("\nProblem " + str(equation['id'])+ ": " + equation['problem'][:-1] + " = " + str(eval(equation['problem'][:-1])))
